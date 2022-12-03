6e4b2329d52860407a7ab37ebfb0928c88f845be
Marcos Duarte <pe.marcos30@gmail.com>
2022-12-02 22:52:36 -0300

    SideBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { DefaultComponent } from './admin/default/default.component';
+import { SidebarComponent } from './admin/shard/sidebar/sidebar.component';
+
+const Ux_modules = [
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatToolbarModule,
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DefaultComponent, SidebarComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, Ux_modules],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
